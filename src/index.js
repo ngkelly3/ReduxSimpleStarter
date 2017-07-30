0f296de01c9b427cda55bd108cabfaf4d94f1970
Kelly Ng <kellyng87@gmail.com>
2017-07-30 16:46:20 -0700

    Create app constructor and initial data request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -6,16 +6,25 @@ import SearchBar from './components/search_bar';
 // Google API Key that will allow us to make requests to YouTube
 const API_KEY = 'AIzaSyB_LPp1iO9UqxA_HK8DtQ27JNm6MSothpk';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {console.log(data)});
-
 // Create a new component.  This component should produce
 // some HTML (but we haven't sent it to the DOM yet!)
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {this.setState({ videos })});
+
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it on
