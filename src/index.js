1ab0a087fe9bc1432846975d9e3f6a0d7f81c0fa
Kelly Ng <kellyng87@gmail.com>
2017-07-30 16:37:50 -0700

    include YouTube API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // Google API Key that will allow us to make requests to YouTube
 const API_KEY = 'AIzaSyB_LPp1iO9UqxA_HK8DtQ27JNm6MSothpk';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {console.log(data)});
+
 // Create a new component.  This component should produce
 // some HTML (but we haven't sent it to the DOM yet!)
-
 const App = () => {
   return (
     <div>
