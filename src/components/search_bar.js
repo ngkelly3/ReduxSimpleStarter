4f8b1be0ff7061c1fb047caca12517aa36732f04
Kelly Ng <kellyng87@gmail.com>
2017-07-30 17:15:41 -0700

    Add keys to videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        value={this.state.term}
+        {this.state.term}
         <input onChange={event => this.setState({ term: event.target.value })} />
       </div>
 
